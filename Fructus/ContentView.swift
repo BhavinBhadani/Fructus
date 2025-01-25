7d616de1b495b61b04b1e7a0bb806a7f35f53b22
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-25 09:40:30 +0530

    Initial Commit

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Bhavin Bhadani on 25/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
