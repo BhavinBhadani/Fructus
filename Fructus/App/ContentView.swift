3e5ffa85cb70ea30ba818c345941e3434c5b7b9b
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-30 10:49:38 +0530

    Update content view to open settings

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingSettings = false
+    
     var fruits: [Fruit] = fruitsData
     
     var body: some View {
@@ -25,6 +27,18 @@ struct ContentView: View {
             }
             .listStyle(.plain)
             .navigationBarTitle("Fruits")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        isShowingSettings = true
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
+                    }
+                }
+            }
+            .sheet(isPresented: $isShowingSettings) {
+                SettingsView()
+            }
         }
     }
 }
