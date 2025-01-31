e5d048d9670f7d28278f427b91bfd77bd67b5059
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-31 10:46:12 +0530

    Fixed layout for iPadOS

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -14,34 +14,32 @@ struct FruitDetailView: View {
         NavigationStack {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(alignment: .center, spacing: 20) {
+                    FruitHeaderView(fruit: fruit)
+                    
                     VStack(alignment: .leading, spacing: 20) {
-                        FruitHeaderView(fruit: fruit)
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundStyle(fruit.gradientColors[1])
                         
-                        VStack(alignment: .leading, spacing: 20) {
-                            Text(fruit.title)
-                                .font(.largeTitle)
-                                .fontWeight(.heavy)
-                                .foregroundStyle(fruit.gradientColors[1])
-                            
-                            Text(fruit.headline)
-                                .font(.headline)
-                                .multilineTextAlignment(.leading)
-                            
-                            FruitNutrientsView(fruit: fruit)
-                            
-                            Text("Learn more about \(fruit.title)".uppercased())
-                                .fontWeight(.bold)
-                                .foregroundStyle(fruit.gradientColors[1])
-                            
-                            Text(fruit.description)
-                                .multilineTextAlignment(.leading)
-                            
-                            SourceLinkView()
-                                .padding(.top, 10)
-                                .padding(.bottom, 40)
-                        }
-                        .padding(.horizontal, 20)
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        FruitNutrientsView(fruit: fruit)
+                        
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundStyle(fruit.gradientColors[1])
+                        
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
                     }
+                    .padding(.horizontal, 20)
                     .frame(maxWidth: 640, alignment: .center)
                 }
                 .navigationBarTitleDisplayMode(.inline)
