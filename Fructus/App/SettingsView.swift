d3dd15ea11c18145e80843da32e350f97542410f
Bhavin B <bhavinbhadani@yahoo.in>
2025-01-31 10:39:20 +0530

    Final touches

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct SettingsView: View {
     @Environment(\.presentationMode) var presentationMode
     
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
+
     var body: some View {
         NavigationStack {
             ScrollView(.vertical, showsIndicators: false) {
@@ -32,6 +34,36 @@ struct SettingsView: View {
                         }
                     }
                     
+                    GroupBox(
+                        label: SettingsLabelView(title: "Customization", image: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                          .padding(.vertical, 8)
+                          .frame(minHeight: 60)
+                          .layoutPriority(1)
+                          .font(.footnote)
+                          .multilineTextAlignment(.leading)
+
+                        Toggle(isOn: $isOnboarding) {
+                          if isOnboarding {
+                            Text("Restarted".uppercased())
+                              .fontWeight(.bold)
+                              .foregroundColor(Color.green)
+                          } else {
+                            Text("Restart".uppercased())
+                              .fontWeight(.bold)
+                              .foregroundColor(Color.secondary)
+                          }
+                        }
+                        .padding()
+                        .background(
+                          Color(UIColor.tertiarySystemBackground)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                    }
+
                     GroupBox(
                         label: SettingsLabelView(title: "Applications", image: "apps.iphone")
                     ) {
